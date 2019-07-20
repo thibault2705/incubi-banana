06c553e577bd3b7c1ccc142e424922a1157d3dd7
[Incubi] ATPhan <atphan2705@gmail.com>
2019-07-20 11:40:10 +0700

    Add fixed user 'incubi'

--- center/src/main/java/com/banana/center/employee/repository/EmployeeRepository.java
+++ center/src/main/java/com/banana/center/employee/repository/EmployeeRepository.java
@@ -2,12 +2,14 @@ package com.banana.center.employee.repository;
 
 import com.banana.center.employee.model.Employee;
 import com.banana.center.employee.model.Employees;
+import com.banana.center.enumeration.Status;
 
 public class EmployeeRepository {
 	private final Employees employees;
 	
     public EmployeeRepository() {
     	employees = new Employees();
+    	employees.add(new Employee("incubi", Status.ACTIVE));
     }
     
     public Employees list() {
